398b8794f7485a71dae580c4943440e3d5201dd4
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2015-04-26 10:50:21 -0600

    don't reset the time if it is already set

--- gpstime.py
+++ gpstime.py
@@ -15,19 +15,27 @@ except:
 	sys.exit(1)
 
 
+# gpsd.utc looks like: 2015-04-01T23:43:21.000Z
+# extracting the fields should not be needed
+#gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
+
+rv = 0
 while True:
 	gpsd.next()
 	if gpsd.utc:
-		# extracting the fields should not be needed
 		gpstime = gpsd.utc[0:19]
-		#gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
-		print 'Setting system time to GPS time: %s UTC' % gpstime
+		systime = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime())
+		print '     GPS time: %s\n  System time: %s' % (gpstime,systime)
+		if gpstime == systime:
+			print 'System time matches GPS time. Not resetting.'
+			break
+		print 'Setting system time to GPS time'
 		rv = os.system('date --utc --set="%s"' % gpstime)
 		if rv:
 			print '0x%04x: Command failed. TIME NOT SET.' % rv
-			sys.exit(2)
+			break
 		print 'System time set.'
 		break
 	time.sleep(1)
 
-sys.exit(0)
+sys.exit(rv)
