8fe1d1189363691c2396d66f654aa1fb4f1b0c3c
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2015-04-25 21:47:21 -0600

    No more sudo! Add error handling, other minor fixups

--- gpstime.py
+++ gpstime.py
@@ -1,22 +1,31 @@
+#!/usr/bin/python
+
 import os
 import sys
 import time
 from gps import *
 
-print 'Attempting to access GPS time...'
+
+print 'Attempting to acquire GPS time...'
 
 try:
 	gpsd = gps(mode=WATCH_ENABLE)
 except:
-	print 'No GPS connection present. TIME NOT SET.'
-	sys.exit()
+	print 'No GPSd connection. TIME NOT SET.'
+	sys.exit(1)
+
 
 while True:
 	gpsd.next()
-	if gpsd.utc != None and gpsd.utc != '':
+	if gpsd.utc:
 		gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
-		print 'Setting system time to GPS time...'
-		os.system('sudo date --set="%s"' % gpstime)
+		print 'Setting system time to GPS time: %s UTC' % gpstime
+		rv = os.system('date --utc --set="%s"' % gpstime)
+		if rv:
+			print '0x%04x: Command failed. TIME NOT SET.' % rv
+			sys.exit(2)
 		print 'System time set.'
-		sys.exit()
+		break
 	time.sleep(1)
+
+sys.exit(0)
