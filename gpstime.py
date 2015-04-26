a035b55e1fb419203e8aa9ea5a8b27d67a7d4e39
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2015-04-26 14:11:48 -0600

    Don't use os.system (launches shell then date) only subprocess if needed Prefer to use libc.settimeofday() directly

--- gpstime.py
+++ gpstime.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
-import os
 import sys
 import time
 from gps import *
+
+USE_DATE_COMMAND = False
+#USE_DATE_COMMAND = True
+
 
 
 print 'Attempting to acquire GPS time...'
@@ -15,9 +18,33 @@ except:
 	sys.exit(1)
 
 
+if USE_DATE_COMMAND:
+	from subprocess import call
+	# timestr looks like:  2015-04-01T23:43:21
+	# extracting the fields should not be needed for 'date'
+	def set_time(timestr):
+		#timestr = timestr[0:4] + timestr[5:7] + timestr[8:10] + ' ' + timestr[11:19]
+		return call(['date', '--utc', '--set', timestr])
+else:
+	# man 2 settimeofday
+	import ctypes
+	import ctypes.util
+	import struct
+	from calendar import timegm
+	libc = ctypes.CDLL(ctypes.util.find_library('c'))
+	def set_time(timestr):
+		tsec = timegm(time.strptime(timestr, '%Y-%m-%dT%H:%M:%S'))
+		# nanoseconds delay from gps msg to set - a fractional second
+		tfrac = 800000000
+		tpack = struct.pack('2q', tsec, tfrac)
+		# (Q=u64, q=s64, l=s32, L=u32)
+		return libc.settimeofday(tpack, None)
+		#libc.gettimeofday(tpack, None)
+
+
+
+
 # gpsd.utc looks like: 2015-04-01T23:43:21.000Z
-# extracting the fields should not be needed
-#gpstime = gpsd.utc[0:4] + gpsd.utc[5:7] + gpsd.utc[8:10] + ' ' + gpsd.utc[11:19]
 
 rv = 0
 while True:
@@ -30,12 +57,12 @@ while True:
 			print 'System time matches GPS time. Not resetting.'
 			break
 		print 'Setting system time to GPS time'
-		rv = os.system('date --utc --set="%s"' % gpstime)
+		rv = set_time(gpstime)
 		if rv:
-			print '0x%04x: Command failed. TIME NOT SET.' % rv
+			print '0x%04x: Command failed. TIME NOT SET.' % (0xffff & rv)
 			break
 		print 'System time set.'
 		break
-	time.sleep(1)
+	time.sleep(0.4)
 
 sys.exit(rv)
